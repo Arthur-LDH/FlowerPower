fed2c109cad5c16cd807b968c389f43f4df44945
Louise EGAIN <louiseegain2401@gmail.com>
2024-04-05 11:01:51 +0200

    update : trigger and functions ok

---
+++ src/sql/functions_orders.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION calculate_average_basket()
+    RETURNS NUMERIC AS $$
+DECLARE
+    average_basket NUMERIC;
+BEGIN
+    SELECT AVG(total) INTO average_basket FROM "db_orders"."order";
+    RETURN average_basket;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT calculate_average_basket();
